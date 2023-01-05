de3695c5645887a590d6d689f1d983db83343dfe
Victor Rodrigues <vr33209@gmail.com>
2023-01-05 18:32:37 -0300

    ensure RemoteAuthentication calls HttpClient with correct body

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -2,30 +2,39 @@ import 'package:faker/faker.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
+import 'package:fordev/domain/usecases/authentication.dart';
+
 class RemoteAuthentication {
   final HttpClient httpClient;
   final String url;
   final String method;
+  final Map<dynamic, dynamic>? body;
 
   RemoteAuthentication({
     required this.httpClient,
     required this.url,
     required this.method,
+    this.body,
   });
 
-  Future<void>? auth() async {
-    await httpClient.request(url: url, method: method);
+  Future<void> auth(AuthenticationParams params) async {
+    final body = {'email': params.email, 'password': params.secret};
+    await httpClient.request(url: url, method: method, body: body);
   }
 }
 
 abstract class HttpClient {
-  Future<void>? request({required String url, required String method}) async {}
+  Future<void>? request({
+    required String url,
+    required String method,
+    Map<dynamic, dynamic>? body,
+  }) async {}
 }
 
 class HttpClientSpy extends Mock implements HttpClient {}
 
 void main() {
-  late RemoteAuthentication? sut;
+  late RemoteAuthentication sut;
   late HttpClientSpy httpClient;
   late String url;
   setUp(() {
@@ -35,8 +44,14 @@ void main() {
         RemoteAuthentication(httpClient: httpClient, url: url, method: 'post');
   });
   test('Should call HttpClient with correct values', () async {
-    await sut?.auth();
+    final params = AuthenticationParams(
+        email: faker.internet.email(), secret: faker.internet.password());
 
-    verify(httpClient.request(url: url, method: 'post'));
+    await sut.auth(params);
+
+    verify(httpClient.request(
+        url: url,
+        method: 'post',
+        body: {'email': params.email, 'password': params.secret}));
   });
 }
