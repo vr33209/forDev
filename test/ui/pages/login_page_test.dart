b43ba4ae9b691ea6d25d503591af783fdbc85007
Victor Rodrigues <vr33209@gmail.com>
2023-01-09 16:51:43 -0300

    create test to load with correct initial value

---
+++ test/ui/pages/login_page_test.dart
@@ -0,0 +1,32 @@
+// ignore_for_file: deprecated_member_use
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:fordev/ui/pages/login_page.dart';
+
+void main() {
+  testWidgets('Should load with correct initial value',
+      (WidgetTester tester) async {
+    const loginPage = MaterialApp(
+      home: Scaffold(
+        body: LoginPage(),
+      ),
+    );
+
+    await tester.pumpWidget(loginPage);
+
+    final emailTextChildren = find.descendant(
+        of: find.bySemanticsLabel('Email'), matching: find.byType(Text));
+
+    expect(emailTextChildren, findsOneWidget);
+
+    final passwordTextChildren = find.descendant(
+        of: find.bySemanticsLabel('Senha'), matching: find.byType(Text));
+
+    expect(passwordTextChildren, findsOneWidget);
+
+    final button = tester.widget<RaisedButton>(find.byType(RaisedButton));
+
+    expect(button.onPressed, null);
+  });
+}
