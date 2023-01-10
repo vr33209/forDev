31f0327b68baef1238cc938f59d4806d85ed0f90
Victor Rodrigues <vr33209@gmail.com>
2023-01-10 20:39:15 -0300

    ensure EmailValidation returns error if email is invalid

--- test/validation/validators/email_validation_test.dart
+++ test/validation/validators/email_validation_test.dart
@@ -1,3 +1,4 @@
+import 'package:faker/faker.dart';
 import 'package:fordev/validation/protocols/field_validation.dart';
 import 'package:test/test.dart';
 
@@ -5,7 +6,17 @@ class EmailValidation implements FieldValidation {
   final String email;
 
   EmailValidation(this.email);
-  String? validate(String? value) {}
+
+  @override
+  String? validate(String? value) {
+    final emailRegex = RegExp(
+        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+
+    final isValid =
+        value?.isNotEmpty != true || emailRegex.hasMatch(value ?? '');
+
+    return isValid ? null : 'Campo inválido.';
+  }
 }
 
 void main() {
@@ -15,14 +26,18 @@ void main() {
     sut = EmailValidation('any_field');
   });
   test('Should return null if email is empty', () {
-    final error = sut.validate('');
-
-    expect(error, null);
+    expect(sut.validate(''), null);
   });
 
   test('Should return null if email is null', () {
-    final error = sut.validate(null);
+    expect(sut.validate(null), null);
+  });
 
-    expect(error, null);
+  test('Should return null if email is valid', () {
+    expect(sut.validate(faker.internet.email()), null);
+  });
+
+  test('Should return error if email is invalid', () {
+    expect(sut.validate('invalid_email'), 'Campo inválido.');
   });
 }
